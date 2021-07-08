88618faf690638c5c767a1c1ae39a54f95127f4c
Julien Gormotte <jgormotte@ate.info>
2021-07-08 17:46:20 +0200

    template html

--- ferie.py
+++ ferie.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from typing import List
 from workalendar.europe import France, FranceAlsaceMoselle
 from datetime import datetime
@@ -6,6 +6,7 @@ import locale
 
 locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
 app = Flask(__name__)
+INDEX = "index.html"
 TODAY = datetime.today().date()
 
 
@@ -23,19 +24,29 @@ def get_next_date(feries: List[datetime]) -> datetime:
         return [_[0] for _ in France().holidays(TODAY.year + 1)][0]
 
 
-@app.route("/")
-def ferie():
-    feries = [_[0] for _ in France().holidays(TODAY.year)]
+def define_message(feries: List[datetime]) -> str:
+    """Defines message for HTML page
+
+    :param feries: list of holidays
+    :type feries: List[datetime]
+    :return: message
+    :rtype: str
+    """
     if TODAY in feries:
         return "Oui, profitez-en bien !"
     else:
         return f"Non, malheureusement, vivement le {get_next_date(feries)}"
+
+
+@app.route("/")
+def ferie():
+    feries = [_[0] for _ in France().holidays(TODAY.year)]
+    message = define_message(feries)
+    return render_template(INDEX, message=message, alsace=False)
 
 
 @app.route("/alsace-moselle")
 def ferie_am():
     feries = [_[0] for _ in FranceAlsaceMoselle().holidays(TODAY.year)]
-    if TODAY in feries:
-        return "Oui, profitez-en bien !"
-    else:
-        return f"Non, malheureusement, vivement le {get_next_date(feries)}"
+    message = define_message(feries)
+    return render_template(INDEX, message=message, alsace=True)
