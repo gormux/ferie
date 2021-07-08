10efa936e7a74b4e62b9e63df210d773c9da512f
Julien Gormotte <jgormotte@ate.info>
2021-07-08 16:48:50 +0200

    support alsace-moselle

--- ferie.py
+++ ferie.py
@@ -1,21 +1,41 @@
 from flask import Flask
-from workalendar.europe import France
+from typing import List
+from workalendar.europe import France, FranceAlsaceMoselle
 from datetime import datetime
 import locale
 
 locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
 app = Flask(__name__)
+TODAY = datetime.today().date()
+
+
+def get_next_date(feries: List[datetime]) -> datetime:
+    """Gets next holiday date
+
+    :param feries: list of holidays
+    :type feries: List[datetime]
+    :return: next holiday
+    :rtype: datetime
+    """
+    try:
+        return next(_ for _ in feries if TODAY < _).strftime(r"%d %B")
+    except StopIteration:
+        return [_[0] for _ in France().holidays(TODAY.year + 1)][0]
 
 
 @app.route("/")
 def ferie():
-    today = datetime.today().date()
-    feries = [_[0] for _ in France().holidays(today.year)]
-    try:
-        next_date = next(_ for _ in feries if today < _).strftime(r"%d %B")
-    except StopIteration:
-        next_date = [_[0] for _ in France().holidays(today.year + 1)][0]
-    if today in feries:
+    feries = [_[0] for _ in France().holidays(TODAY.year)]
+    if TODAY in feries:
         return "Oui, profitez-en bien !"
     else:
-        return f"Non, malheureusement, vivement le {next_date}"
+        return f"Non, malheureusement, vivement le {get_next_date(feries)}"
+
+
+@app.route("/alsace-moselle")
+def ferie_am():
+    feries = [_[0] for _ in FranceAlsaceMoselle().holidays(TODAY.year)]
+    if TODAY in feries:
+        return "Oui, profitez-en bien !"
+    else:
+        return f"Non, malheureusement, vivement le {get_next_date(feries)}"
